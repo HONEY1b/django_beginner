6fda41f6164fdc1493e961d4ba27a0409731703b
Nhoney1b <nh822@naver.com>
2022-04-06 22:29:26 +0900

    CRUD - Read

---
+++ django/myapp/urls.py
@@ -0,0 +1,13 @@
+"""myapp URL Configuration
+
+
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('', views.index),
+	path('create/', views.create),
+	path('read/<id>/', views.read)
+]
--- django/myapp/views.py
+++ django/myapp/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+import random
 
-# Create your views here.
+topics = [
+	{'id':1, 'title' : 'routing', 'body':'routing is...'},
+	{'id':2, 'title' : 'view', 'body':'view is...'},
+	{'id':3, 'title' : 'model', 'body':'model is...'}
+]
+
+
+def index(request):
+	global topics
+	ol = ''
+	
+	for topic in topics:
+		ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+	
+	return HttpResponse(f'''
+	<html>
+	<body>
+		<h1>Django</h1>
+		<ol>
+			{ol}
+		</ol>
+		<h2><Welcome/h2>
+		Hello,Django
+	</body>
+	</html>
+	
+	''')
+
+def create(request):
+
+	
+	return HttpResponse('Create')
+	
+def read(request, id):
+
+	return HttpResponse('Read'+id)
